edb9d4548a95acf2a545c42589e6c751283b387c
yrtc99 <yrtc99@gmail.com>
2023-03-17 21:39:43 +0800

    update

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { formatUnits, parseUnits } from "ethers/lib/utils";
 
-const Balance = () => {
+import styles from "../styles";
+
+const Balance = ({ tokenBalance }) => {
   return (
-    <div>
-      
+    <div className={styles.balance}>
+      <p className={styles.balanceText}>
+        {tokenBalance ? (
+          <>
+            <span className={styles.balanceBold}>Balance: </span>
+            {formatUnits(tokenBalance ?? parseUnits("0"))}
+          </>
+        ) : (
+          ""
+        )}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Balance
+export default Balance;
